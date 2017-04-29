93408d093994dc44d9334e35651419460be12587
Katerina Koukiou <k.koukiou@gmail.com>
2017-04-29 15:17:44 +0200

    Deleted backend.* files. Not used.

--- backend.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INTER_
-#define _INTER_
-#include "inter.h"
-#endif
-
-Scope n_cur;
-/*Συνάρτηση που παίρνει μία λίστα από τετάδες και την μετατρέπει σε κώδικα 8085 assembly*/
-void Quads_to_Assembly(qnode ** head); 
